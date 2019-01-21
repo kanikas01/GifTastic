fbb1ba644d8b4365234f9ab227aaf5d107ff12ec
kanikas01 <larvakurt@yahoo.com>
2019-01-21 12:37:33 -0800

    Add basic pseudocode outline

---
+++ assets/javascript/main.js
@@ -0,0 +1,19 @@
+// Create array of foods
+
+//  For food in food array
+//    add food button to page      
+
+// Function which adds new button to page
+
+// Create ajax request for a given food
+
+// Display new food gifs on page with rating above each gif
+
+// If you click on still image it animates
+// If you click on animated image, it stops
+
+// event handlers for:
+// button - add button to page
+// gif button - get gifs and add to page
+// image - click to start or stop animation
+
