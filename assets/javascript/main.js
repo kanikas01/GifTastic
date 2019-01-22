a265b9263a7c4fc12e35310e8577d83e963ef5c4
kanikas01 <larvakurt@yahoo.com>
2019-01-21 18:20:43 -0800

    Add code to add button event listener and addButton function

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -8,16 +8,23 @@ $(document).ready(function () {
   var $foodsDiv = $("#foods");
 
   // Create array of foods
-  var foods = [];
+  var foods = ["cow"];
 
   //  For food in food array
-  //    add food button to page   
+  //    add food button to page 
+  
+  foods.forEach(addButton);
 
 
   // ---------- Event listeners ---------- //
 
   // button - add button to page
-  $('#add-food').on('click', addButton);
+  $('#add-food').on('click', function(event) {
+    event.preventDefault();
+    var newFood = $("#food-input").val();
+    addButton(newFood);
+    $("#food-input").val("");
+  });
 
   // gif button - get gifs and add to page
   $('#food-button').on('click', addGifs);
@@ -29,8 +36,8 @@ $(document).ready(function () {
   // ---------- Event handlers ---------- //
 
   // Add new button to page
-  function addButton() {
-
+  function addButton(food) {
+    console.log(food);
   }
 
   // Display new food gifs on page with rating above each gif
