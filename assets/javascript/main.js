523de10d919fca32d0dbced3299c1bfcdd06c8ac
kanikas01 <larvakurt@yahoo.com>
2019-01-21 16:38:49 -0800

    Add global variables, empty functions, event handlers and event listeners

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,31 +1,56 @@
-// ---------- Global variables ---------- //
 
-// Create array of foods
-//  For food in food array
-//    add food button to page   
+$(document).ready(function () {
 
-// Create variables for page divs
+  // ---------- Global variables ---------- //
 
+  var $foodButtonsDiv = $("#food-buttons");
+  var $foodFormDiv = $("#food-form");
+  var $foodsDiv = $("#foods");
 
-// ---------- Event listeners ---------- //
+  // Create array of foods
+  var foods = [];
 
-// button - add button to page
-// gif button - get gifs and add to page
-// image - click to start or stop animation
-
-
-// ---------- Event handlers ---------- //
-// Function which adds new button to page
-
-// Create ajax request for a given food
-
-// Display new food gifs on page with rating above each gif
-
-// If you click on still image it animates
-// If you click on animated image, it stops
+  //  For food in food array
+  //    add food button to page   
 
 
-// ---------- Helper functions ---------- //
+  // ---------- Event listeners ---------- //
+
+  // button - add button to page
+  $('#add-food').on('click', addButton);
+
+  // gif button - get gifs and add to page
+  $('#food-button').on('click', addGifs);
+
+  // image - click to start or stop animation
+  $foodsDiv.on('click', '.image', toggleImage);
 
 
+  // ---------- Event handlers ---------- //
+
+  // Add new button to page
+  function addButton() {
+
+  }
+
+  // Display new food gifs on page with rating above each gif
+  function addGifs() {
+    clear();
+    // Create ajax request for a given food
+  }
+  
+  function toggleImage() {
+    // If you click on still image it animates
+    // If you click on animated image, it stops
+
+  }
+  
+
+  // ---------- Helper functions ---------- //
+
+  function clear() {
+    $foodsDiv.empty();
+  }
+
+});
 
